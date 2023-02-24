f324ca3a8f132be912f35f49ee113d83b3abedb1
Sowmya-manthati <sowmyamanthati30@gmail.com>
2023-02-24 20:47:58 +0000

    first spring lesson commit

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CashCardApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testing git commit");
 		SpringApplication.run(CashCardApplication.class, args);
 	}
 
